64621279cb396277f4c8ca3f9778aef862b55c1c
Anthony Chablov <achablov@myseneca.ca>
2023-04-18 15:41:26 -0400

    update app layout

---
+++ src/components/App/AppLayout.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar/Sidebar";
+import Card from "./Card/Card";
+
+const AppLayout = () => {
+  return (
+    <div>
+        
+
+    </div>
+  )
+}
+
+export default AppLayout
